24b501e9f3e317ae6d0f61cbfc9a74a5bdd1bcb7
Nguyễn Xuân Thanh <131781167+ThanhNX0401@users.noreply.github.com>
2024-10-31 14:03:42 +0700

    2.0

--- Tutorials/04_sentence_recognition/model.py
+++ Tutorials/04_sentence_recognition/model.py
@@ -11,29 +11,27 @@ def train_model(input_dim, output_dim, activation="leaky_relu", dropout=0.2):
     # normalize images here instead in preprocessing step
     input = layers.Lambda(lambda x: x / 255)(inputs)
 
-    x1 = residual_block(input, 32, activation=activation, skip_conv=True, strides=1, dropout=dropout)
+    x1 = residual_block(input, 64, activation=activation, skip_conv=True, strides=1, dropout=dropout)
 
-    x2 = residual_block(x1, 32, activation=activation, skip_conv=True, strides=2, dropout=dropout)
-    x3 = residual_block(x2, 32, activation=activation, skip_conv=False, strides=1, dropout=dropout)
+    x2 = residual_block(x1, 128, activation=activation, skip_conv=True, strides=2, dropout=dropout)
+    x3 = residual_block(x2, 128, activation=activation, skip_conv=False, strides=1, dropout=dropout)
 
-    x4 = residual_block(x3, 64, activation=activation, skip_conv=True, strides=2, dropout=dropout)
-    x5 = residual_block(x4, 64, activation=activation, skip_conv=False, strides=1, dropout=dropout)
+    x4 = residual_block(x3, 256, activation=activation, skip_conv=True, strides=2, dropout=dropout)
+    x5 = residual_block(x4, 256, activation=activation, skip_conv=False, strides=1, dropout=dropout)
 
-    x6 = residual_block(x5, 128, activation=activation, skip_conv=True, strides=2, dropout=dropout)
-    x7 = residual_block(x6, 128, activation=activation, skip_conv=True, strides=1, dropout=dropout)
+    x6 = residual_block(x5, 512, activation=activation, skip_conv=True, strides=2, dropout=dropout)
+    x7 = residual_block(x6, 512, activation=activation, skip_conv=True, strides=1, dropout=dropout) #1024 maybe
 
-    x8 = residual_block(x7, 128, activation=activation, skip_conv=True, strides=2, dropout=dropout)
-    x9 = residual_block(x8, 128, activation=activation, skip_conv=False, strides=1, dropout=dropout)
 
-    squeezed = layers.Reshape((x9.shape[-3] * x9.shape[-2], x9.shape[-1]))(x9)
+    squeezed = layers.Reshape((x7.shape[-3] * x7.shape[-2], x7.shape[-1]))(x7)
 
-    blstm = layers.Bidirectional(layers.LSTM(256, return_sequences=True))(squeezed)
-    blstm = layers.Dropout(dropout)(blstm)
+    blstm1 = layers.Bidirectional(layers.LSTM(512, return_sequences=True))(squeezed)
+    blstm1 = layers.Dropout(dropout)(blstm1)
 
-    blstm = layers.Bidirectional(layers.LSTM(64, return_sequences=True))(blstm)
-    blstm = layers.Dropout(dropout)(blstm)
+    blstm2 = layers.Bidirectional(layers.LSTM(512, return_sequences=True))(blstm1)
+    blstm2 = layers.Dropout(dropout)(blstm2)
 
-    output = layers.Dense(output_dim + 1, activation="softmax", name="output")(blstm)
+    output = layers.Dense(output_dim + 1, activation="softmax", name="output")(blstm2)
 
     model = Model(inputs=inputs, outputs=output)
     return model
--- mltu/transformers.py
+++ mltu/transformers.py
@@ -8,7 +8,7 @@ import importlib
 import numpy as np
 
 from . import Image
-from mltu.annotations.audio import Audio
+
 from mltu.annotations.detections import Detections
 
 """ Implemented Transformers:
@@ -17,8 +17,6 @@ from mltu.annotations.detections import Detections
 - LabelIndexer - Convert label to index by vocab
 - LabelPadding - Pad label to max_word_length
 - ImageNormalizer - Normalize image to float value, transpose axis if necessary and convert to numpy
-- SpectrogramPadding - Pad spectrogram to max_spectrogram_length
-- AudioToSpectrogram - Convert Audio to Spectrogram
 - ImageShowCV2 - Show image for visual inspection
 """
 
@@ -195,138 +193,6 @@ class ImageNormalizer:
             img = img.transpose(2, 0, 1)
         
         return img, annotation
-
-
-class SpectrogramPadding(Transformer):
-    """Pad spectrogram to max_spectrogram_length
-    
-    Attributes:
-        padding_value (int): Value to pad
-        max_spectrogram_length (int): Maximum length of spectrogram. Must be specified if use_on_batch is False. Default: None
-        use_on_batch (bool): Whether to use on batch. Default: False
-    """
-    def __init__(
-        self, 
-        padding_value: int,
-        max_spectrogram_length: int = None, 
-        use_on_batch: bool = False
-        ) -> None:
-        self.max_spectrogram_length = max_spectrogram_length
-        self.padding_value = padding_value
-        self.use_on_batch = use_on_batch
-
-        if not use_on_batch and max_spectrogram_length is None:
-            raise ValueError("max_spectrogram_length must be specified if use_on_batch is False")
-
-    def __call__(self, spectrogram: np.ndarray, label: np.ndarray):
-        if self.use_on_batch:
-            max_len = max([len(a) for a in spectrogram])
-            padded_spectrograms = []
-            for spec in spectrogram:
-                padded_spectrogram = np.pad(spec, ((0, max_len - spec.shape[0]), (0,0)), mode="constant", constant_values=self.padding_value)
-                padded_spectrograms.append(padded_spectrogram)
-
-            padded_spectrograms = np.array(padded_spectrograms)
-            label = np.array(label)
-
-            return padded_spectrograms, label
-
-        padded_spectrogram = np.pad(spectrogram, ((0, self.max_spectrogram_length - spectrogram.shape[0]),(0,0)), mode="constant", constant_values=self.padding_value)
-
-        return padded_spectrogram, label
-
-
-class AudioPadding(Transformer):
-    def __init__(self, max_audio_length: int, padding_value: int = 0, use_on_batch: bool = False, limit: bool = False):
-        super(AudioPadding, self).__init__()
-        self.max_audio_length = max_audio_length
-        self.padding_value = padding_value
-        self.use_on_batch = use_on_batch
-        self.limit = limit
-
-    def __call__(self, audio: Audio, label: typing.Any):
-        # batched padding
-        if self.use_on_batch:
-            max_len = max([len(a) for a in audio])
-            padded_audios = []
-            for a in audio:
-                # limit audio if it exceed max_audio_length
-                padded_audio = np.pad(a, (0, max_len - a.shape[0]), mode="constant", constant_values=self.padding_value)
-                padded_audios.append(padded_audio)
-
-            padded_audios = np.array(padded_audios)
-            # limit audio if it exceed max_audio_length
-            if self.limit:
-                padded_audios = padded_audios[:, :self.max_audio_length]
-
-            return padded_audios, label
-
-        audio_numpy = audio.numpy()
-        # limit audio if it exceed max_audio_length
-        if self.limit:
-            audio_numpy = audio_numpy[:self.max_audio_length]
-        padded_audio = np.pad(audio_numpy, (0, self.max_audio_length - audio_numpy.shape[0]), mode="constant", constant_values=self.padding_value)
-
-        audio.audio = padded_audio
-
-        return audio, label
-
-class AudioToSpectrogram(Transformer):
-    """Read wav file with librosa and return audio and label
-    
-    Attributes:
-        frame_length (int): Length of the frames in samples.
-        frame_step (int): Step size between frames in samples.
-        fft_length (int): Number of FFT components.
-        log_level (int): Logging level (default: logging.INFO)
-    """
-    def __init__(
-            self,
-            frame_length: int = 256,
-            frame_step: int = 160,
-            fft_length: int = 384,
-            log_level: int = logging.INFO,
-        ) -> None:
-        super(AudioToSpectrogram, self).__init__(log_level=log_level)
-        self.frame_length = frame_length
-        self.frame_step = frame_step
-        self.fft_length = fft_length
-
-        # import librosa using importlib
-        try:
-            self.librosa = importlib.import_module('librosa')
-            print("librosa version:", self.librosa.__version__)
-        except ImportError:
-            raise ImportError("librosa is required to augment Audio. Please install it with `pip install librosa`.")
-
-    def __call__(self, audio: Audio, label: typing.Any):
-        """Compute the spectrogram of a WAV file
-
-        Args:
-            audio (Audio): Audio object
-            label (Any): Label of audio
-
-        Returns:
-            np.ndarray: Spectrogram of audio
-            label (Any): Label of audio
-        """
-
-        # Compute the Short Time Fourier Transform (STFT) of the audio data and store it in the variable 'spectrogram'
-        # The STFT is computed with a hop length of 'frame_step' samples, a window length of 'frame_length' samples, and 'fft_length' FFT components.
-        # The resulting spectrogram is also transposed for convenience
-        spectrogram = self.librosa.stft(audio.numpy(), hop_length=self.frame_step, win_length=self.frame_length, n_fft=self.fft_length).T
-
-        # Take the absolute value of the spectrogram to obtain the magnitude spectrum
-        spectrogram = np.abs(spectrogram)
-
-        # Take the square root of the magnitude spectrum to obtain the log spectrogram
-        spectrogram = np.power(spectrogram, 0.5)
-
-        # Normalize the spectrogram by subtracting the mean and dividing by the standard deviation.
-        # A small value of 1e-10 is added to the denominator to prevent division by zero.
-        spectrogram = (spectrogram - np.mean(spectrogram)) / (np.std(spectrogram) + 1e-10)
-
-        return spectrogram, label
 
 
 class ImageShowCV2(Transformer):
